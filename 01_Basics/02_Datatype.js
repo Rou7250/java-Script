d26d99a986c21dbc3a02c953f6b7cd73bdecbe25
Roushan Kumar <roushankumarjmp23@gmail.com>
2025-07-31 17:28:58 +0000

    datatype

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,17 @@
+"use Strict"//treat all JS code as newer version
+
+let name="roushan";
+let age=18
+let isLog=false;
+
+//number=>2 to power 53
+//bigint 
+//string =>""
+//boolean => true/false;
+// null => standalone value
+//undefined =>
+// symbol=> unique
+ 
+// object 
+console.log(typeof null);
+
