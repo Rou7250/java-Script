263c7835395a0f759147796df6f4906e9217c737
Roushan Kumar <roushankumarjmp23@gmail.com>
2025-08-01 16:54:17 +0000

    convertion

---
+++ 01_Basics/03_ConversionOpration.js
@@ -0,0 +1,9 @@
+const { useLayoutEffect } = require("react");
+
+ let score="33";
+ 
+ console.log(typeof score);
+ console.log(typeof(score));
+
+let valueno= Number(score);
+console.log(typeof(valueno));
